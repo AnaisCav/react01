15799309ce12af52e26bdc08930f0bcfdeb37339
ANAIS CAVAILLE <anais.cavaille93@gmail.com>
2023-05-31 18:57:59 +0200

    react4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
-function App() {
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+const pokemon = pokemonList[0];
+
+const App = () => {
   return (
-    <div>
-      <PokemonCard />
-    </div>
+    <>
+      <div>
+        <PokemonCard name={pokemon.name} imgSrc={pokemon.imgSrc} />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,40 +1,57 @@
-const pokemonList = [
-  {
-    name: "Bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  const pokemon = pokemonList[0];
-
-  for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].imgSrc != undefined) {
-      if (pokemon === pokemonList[0]) {
-        return (
-          <>
-            <figure>
-              <img src={pokemonList[i].imgSrc} alt={pokemonList[i].name}></img>
-              <figcaption>{pokemonList[i].name}</figcaption>
-            </figure>
-          </>
-        );
-      }
-    } else {
-      return (
-        <>
-          <figure>
-            <p>???</p>
-            <figcaption>{pokemonList[i].name}</figcaption>
-          </figure>
-        </>
-      );
-    }
-  }
+function PokemonCard({ name, imgSrc }) {
+  return (
+    <figure>
+      {imgSrc != null ? (
+        <img src={imgSrc} alt={`Image de ${name}`} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{name}</figcaption>
+    </figure>
+  );
 }
+
+// function PokemonCard({name, imgSrc}) {
+
+//   const pokemon = pokemonList[0];
+//   return (
+//     <figure>
+//       {pokemon.imgSrc != null ? (
+//         <img src={pokemon.imgSrc} alt={pokemon.name} />
+//       ) : (
+//         <p>???</p>
+//       )}
+//       <figcaption>{pokemon.name}</figcaption>
+//     </figure>
+//   );
+// }
+
+// function PokemonCard() {
+//   const pokemon = pokemonList[0];
+
+//   for (let i = 0; i < pokemonList.length; i++) {
+//     if (pokemonList[i].imgSrc != undefined) {
+//       if (pokemon === pokemonList[0]) {
+//         return (
+//           <>
+//             <figure>
+//               <img src={pokemonList[i].imgSrc} alt={pokemonList[i].name}></img>
+//               <figcaption>{pokemonList[i].name}</figcaption>
+//             </figure>
+//           </>
+//         );
+//       }
+//     } else {
+//       return (
+//         <>
+//           <figure>
+//             <p>???</p>
+//             <figcaption>{pokemonList[i].name}</figcaption>
+//           </figure>
+//         </>
+//       );
+//     }
+//   }
+// }
 
 export default PokemonCard;
